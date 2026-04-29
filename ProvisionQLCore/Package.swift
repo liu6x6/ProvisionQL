0f8b77bfe24a6ed09edc7c5d11544525febf8e7c
xiaolongLiu <liu_xiaolong@apple.com>
2026-04-29 11:40:49 +0800

    update for android ARSC

--- ProvisionQLCore/Package.swift
+++ ProvisionQLCore/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19"),
         .package(url: "https://github.com/tsolomko/SWCompression.git", from: "4.8.0"),
-        .package(url: "https://github.com/liu6x6/SwiftAXML.git", revision: "b7e8b99a141fc82da444423731b8b71588d6b6d0"),
+        .package(path: "/Users/spxt666/SwiftAXML"),
     ],
     targets: [
         .target(
